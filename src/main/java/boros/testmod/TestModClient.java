165f9b9e8e82bfa373036e939312c4aea99797ab
Boros <kevstar421@gmail.com>
2024-02-11 02:27:48 -0500

    Initial commit

---
+++ src/main/java/boros/testmod/TestModClient.java
@@ -0,0 +1,12 @@
+package boros.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
